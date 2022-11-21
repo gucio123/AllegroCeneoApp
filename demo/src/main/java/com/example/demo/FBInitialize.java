a46605e4a8f8e4e5cf6f9fdf3f53c2387cb5cd9d
filkipiel <fkipiel@cisco.com>
2022-11-21 21:34:26 +0100

    Odkomentowuje komentarze w .java

--- demo/src/main/java/com/example/demo/FBInitialize.java
+++ demo/src/main/java/com/example/demo/FBInitialize.java
@@ -1,32 +1,32 @@
-//package com.example.demo;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import org.springframework.stereotype.Service;
-//
-//import javax.annotation.PostConstruct;
-//import java.io.FileInputStream;
-//
-//@Service
-//public class FBInitialize {
-//
-//    @PostConstruct
-//    public void initialize() {
-//        try {
-//
-//            FileInputStream serviceAccount =
-//                    new FileInputStream("demo/src/main/resources/serviceAccountKey.json");
-//
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setDatabaseUrl("https://allegroceneo-2ac4f-default-rtdb.europe-west1.firebasedatabase.app")
-//                    .build();
-//
-//            FirebaseApp.initializeApp(options);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
+package com.example.demo;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Service
+public class FBInitialize {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+
+            FileInputStream serviceAccount =
+                    new FileInputStream("demo/src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://allegroceneo-2ac4f-default-rtdb.europe-west1.firebasedatabase.app")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
