93ae090cc34f9ae01d28c7493e5fe1a73b04a52e
Bartosz Gutowski <bartek.gutowski@onet.pl>
2022-11-25 18:13:31 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/main'""
    This reverts commit 1af0a0e7b83891d105ebd5a0b7d1a3d11a17cc9f.

--- demo/src/main/resources/frontend/angular-app/src/app/app.component.ts
+++ demo/src/main/resources/frontend/angular-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Object} from '../object';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+
+  object: Object ={
+  id: 1,
+  name: 'Aspiryna'
+  };
+
+  constructor() { }
+
+  ngOnInit():void{
+  }
 }
