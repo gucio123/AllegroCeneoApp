311c7622fd9f08b6b7eab315b3bef7d654f726bc
Bartosz Gutowski <bartek.gutowski@onet.pl>
2022-12-01 17:50:50 +0100

    basic validation

--- demo/src/main/resources/frontend/angular-app/src/app/login-register/login-register.component.ts
+++ demo/src/main/resources/frontend/angular-app/src/app/login-register/login-register.component.ts
@@ -18,6 +18,8 @@ export class LoginRegisterComponent implements OnInit{
   constructor(public userAccountService: UseraccountserviceService){ }
   public createAccount(): void {
     // this.userAccountService.createAccount(this.user);  TO NIE WYSTARCZA NA POSTA
+
+    if(this.user.email.includes("@") && this.user.password.length >= 6)
     this.userAccountService // TRZEBA TO ZROBIC TAK, CZYLI DODAC SUBSCRIBE()
       .createAccount(this.user)
       .subscribe()
