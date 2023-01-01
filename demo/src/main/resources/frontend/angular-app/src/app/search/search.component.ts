bee932603e03c51d1e3f63814c6306b68fdcc2cd
barte <bartek.gutowski@onet.pl>
2023-01-01 11:59:44 +0100

    pobieranie z pliku ready

--- demo/src/main/resources/frontend/angular-app/src/app/search/search.component.ts
+++ demo/src/main/resources/frontend/angular-app/src/app/search/search.component.ts
@@ -12,26 +12,30 @@ export class SearchComponent {
   szukajnik: any;
   results: any;
 
-
   constructor(private router: Router, private http: HttpClient) {
   }
+  fileContent: string = '';
 
-  fileContent: string | ArrayBuffer | null = '';
-
-  public onChange(fileList: FileList): void {
-    let file = fileList[0];
+  public onChange(fileList: Event): void {
+    // @ts-ignore
+    let file = (<HTMLInputElement>fileList.target).files[0];
     let fileReader: FileReader = new FileReader();
     let self = this;
-    fileReader.onloadend = function (x) {
-      self.fileContent = fileReader.result;
+    fileReader.onloadend = function(x) {
+      self.szukajnik = fileReader.result;
+      self.search();
+
     }
     fileReader.readAsText(file);
   }
-
-  //nawiguj do danej strony
   condition: any;
   search() {
     this.router.navigate(['/products'], {queryParams: {search: this.szukajnik}});
 
+  }
+  refresh(){
+    this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['/products'], {queryParams: {search: this.szukajnik}});
+    });
   }
 }
